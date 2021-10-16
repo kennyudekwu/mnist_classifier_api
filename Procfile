fc8dedeb2b525f256e7c8864adbf79172e30c999
Kennyudekwu <kennyudekwu@gmail.com>
2021-10-16 19:06:19 +0100

    docs: Update settings.py to suit env vars

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn mnist_api.wsgi --log-file -
+release: python manage.py collectstatic --noinput
--- mnist_api/settings.py
+++ mnist_api/settings.py
@@ -1,7 +1,11 @@
 import django_heroku
+import environ
 import os
 from pathlib import Path
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,12 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#--$+^a_2j(frj3=j55oq_sejt_lxkdwdmvoon$-_w!iqws6xb'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
